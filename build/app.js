a00f7e09f263d50cf34c38c45e4b79b97e8d7db8
Boubacar Barry <Boubacar Barry>
2016-10-22 16:26:27 +0200

    finish step 2 bot

--- build/app.js
+++ build/app.js
@@ -65,16 +65,49 @@ bot.dialog('/', [function (session) {
     }
 }]);
 
-bot.dialog('/cheap', [function (session) {
+bot.dialog('/cheap', [
+/* Step 1 */
+function (session) {
     session.send('What do you like to wear in your free time?');
     // Ask the user to select an item from a carousel.
     var msg = new builder.Message(session).attachmentLayout(builder.AttachmentLayout.carousel).attachments([new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_e.pjpeg").tap(builder.CardAction.showImage(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_e.pjpeg"))]).buttons([builder.CardAction.imBack(session, "select:100", "Select")]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_d.pjpeg").tap(builder.CardAction.showImage(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_d.pjpeg"))]).buttons([builder.CardAction.imBack(session, "select:101", "Select")]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_c.pjpeg").tap(builder.CardAction.showImage(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_c.pjpeg"))]).buttons([builder.CardAction.imBack(session, "select:102", "Select")])]);
     builder.Prompts.choice(session, msg, "select:100|select:101|select:102");
-}, function (session, results) {
+}, function (session, results, next) {
     var action, item;
     var kvPair = results.response.entity.split(':');
     if (!session.userData.selected) {
         session.userData.selected = [];
     }
     session.userData.selected.push(kvPair[1]);
+    next();
+},
+/* Step 2*/
+function () {
+    session.send("What do you wear to work? ");
+
+    var msg = new builder.Message(session).attachmentLayout(builder.AttachmentLayout.carousel).attachments([new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2045_728x972_business.pjpeg").tap(builder.CardAction.imBack(session, "business"))]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/work_relaxed_2.pjpeg").tap(builder.CardAction.imBack(session, "casual"))]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2045_728x972_modernclassic02.pjpeg").tap(builder.CardAction.imBack(session, "business"))]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/work_casual.pjpeg").tap(builder.CardAction.imBack(session, "casual"))])]);
+    builder.Prompts.choice(session, msg, "casual|business");
+}, function (session, results, next) {
+    var response = results.response.entity;
+    var CASUAL = 'casual';
+    var business = 'business';
+    session.userData.selected.push(response);
+
+    if (response === CASUAL) {
+        session.beginDialog('/cheap-casual');
+    } else {
+        session.beginDialog('/cheap-business');
+    }
 }]);
+
+bot.dialog('/cheap-casual', [
+/* Step cheap casual part */
+function (session) {
+    session.send('What do you like to wear in your free time?');
+}, function (session, results, next) {}]);
+
+bot.dialog('/cheap-casual', [
+/* Step cheap business part */
+function (session) {
+    session.send('What do you like to wear in your free time?');
+}, function (session, results, next) {}]);
