8126ccf25d77ea9d9ee7298e681c7c6de770e0a9
Boubacar Barry <Boubacar Barry>
2016-10-22 23:23:19 +0200

    fix request

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
 
 //=========================================================
 // Bot Setup
