fbeec479a6aabcd9765aab1cfcea23cdb9c623f3
Boubacar Barry <Boubacar Barry>
2016-10-22 15:54:08 +0200

    add herocard test

--- build/app.js
+++ build/app.js
@@ -58,7 +58,7 @@ bot.dialog('/', [function (session) {
     builder.Prompts.confirm(session, 'Hi! Are your looking for a new outfit ?');
 }, function (session, results) {
     var response = results.response;
-    if (response === "No") {
+    if (response.toLowerCase() === "no") {
         session.endDialog();
     } else {
         session.beginDialog('/cheap');
@@ -68,34 +68,13 @@ bot.dialog('/', [function (session) {
 bot.dialog('/cheap', [function (session) {
     session.send('What do you like to wear in your free time?');
     // Ask the user to select an item from a carousel.
-    // var msg = new builder.Message(session)
-    //     .attachmentLayout(builder.AttachmentLayout.carousel)
-    //     .attachments([
-    //        new builder.CardImage(session)
-    //         .url("")
-    //         .postBack(session,"select:102", "Select")
-    //     ]);
-    // Ask the user to select an item from a carousel.
-    var msg = new builder.Message(session).attachmentLayout(builder.AttachmentLayout.carousel).attachments([new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_e.pjpeg").tap(builder.CardAction.showImage(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_e.pjpeg"))]).buttons([builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle", "Wikipedia"), builder.CardAction.imBack(session, "select:100", "Select")]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg").tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/800px-PikePlaceMarket.jpg"))]).buttons([builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Pike_Place_Market", "Wikipedia"), builder.CardAction.imBack(session, "select:101", "Select")]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/320px-Night_Exterior_EMP.jpg").tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/800px-Night_Exterior_EMP.jpg"))]).buttons([builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/EMP_Museum", "Wikipedia"), builder.CardAction.imBack(session, "select:102", "Select")])]);
+    var msg = new builder.Message(session).attachmentLayout(builder.AttachmentLayout.carousel).attachments([new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_e.pjpeg").tap(builder.CardAction.showImage(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_e.pjpeg"))]).buttons([builder.CardAction.imBack(session, "select:100", "Select")]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_d.pjpeg").tap(builder.CardAction.showImage(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_d.pjpeg"))]).buttons([builder.CardAction.imBack(session, "select:101", "Select")]), new builder.HeroCard(session).images([builder.CardImage.create(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_c.pjpeg").tap(builder.CardAction.showImage(session, "https://www.outfittery.com/funnels/new/img/thumb__questionnaire_picture/dt-2360_728x972_c.pjpeg"))]).buttons([builder.CardAction.imBack(session, "select:102", "Select")])]);
     builder.Prompts.choice(session, msg, "select:100|select:101|select:102");
 }, function (session, results) {
     var action, item;
     var kvPair = results.response.entity.split(':');
-    switch (kvPair[0]) {
-        case 'select':
-            action = 'selected';
-            break;
+    if (!session.userData.selected) {
+        session.userData.selected = [];
     }
-    switch (kvPair[1]) {
-        case '100':
-            item = "the Space Needle";
-            break;
-        case '101':
-            item = "Pikes Place Market";
-            break;
-        case '102':
-            item = "the EMP Museum";
-            break;
-    }
-    session.endDialog('You %s "%s"', action, item);
+    session.userData.selected.push(kvPair[1]);
 }]);
